3b3e8f0804cd37233a03164235229f539c352965
marcusmolchany <marcusmolchany@gmail.com>
2016-07-01 15:22:03 -0700

    Initial commit
    Add README
    Add demo index.html file
    Add inital jquery plugin code

---
+++ README.md
@@ -0,0 +1,7 @@
+# jquery.filterjitsu.js
+
+## Demo
+```shell
+ $ python -m SimpleHTTPServer 3000
+```
+Navigate to [http://localhost:3000](http://localhost:3000)
---
+++ jquery.filterjitsu.js
@@ -0,0 +1,13 @@
+(function(factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD
+        define(['jquery'], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        factory(require('jquery'));
+    } else {
+        // Browser globals
+        factory(jQuery);
+    }
+}(function($) {
+  console.log($);
+}));
