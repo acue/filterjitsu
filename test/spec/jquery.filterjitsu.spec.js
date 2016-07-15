a43ae0a1356f4e008252351689b6c7127e05f02c
marcusmolchany <marcusmolchany@gmail.com>
2016-07-15 15:50:17 -0700

    Add test for alert

--- test/spec/jquery.filterjitsu.spec.js
+++ test/spec/jquery.filterjitsu.spec.js
@@ -26,4 +26,17 @@ describe('test test', function () {
   it('should hide land items', function () {
     expect($('[data-filterable][data-type!="Water"]')).toBeHidden();
   });
+
+  it('should show an alert', function () {
+    expect($('[data-alert]')).toBeVisible();
+  });
+
+  it('should show an alert with detailed text', function () {
+    expect($('[data-alert]')).toContainHtml(
+      '<div id="info" class="alert alert-info text-center col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">' +
+      '  You are viewing only Water items.' +
+      '  <a href="/context.html">View all items.</a>' +
+      '</div>'
+    );
+  });
 });
