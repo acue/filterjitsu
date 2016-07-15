39f326c5f768ff3427742394b4911b4ac077f17f
marcusmolchany <marcusmolchany@gmail.com>
2016-07-15 12:25:50 -0700

    Add test for hide and show of elements

--- test/spec/jquery.filterjitsu.spec.js
+++ test/spec/jquery.filterjitsu.spec.js
@@ -4,14 +4,26 @@ jasmine.getFixtures().fixturesPath = "base/test/fixtures";
 
 describe('test test', function () {
   var $fj;
-  loadFixtures('template.html');
 
-  // HACK (marcus): the following line of code is needed to mock search query params with phantom js
-  // http://stackoverflow.com/questions/2494213/changing-window-location-without-triggering-refresh
-  window.history.replaceState( {}, '', 'http://localhost:8080/context.html?type=Water');
+  beforeEach(function () {
+    loadFixtures('template.html');
 
-  it('should filter Water items', function () {
+    // HACK (marcus): the following line of code is needed to mock search query params with phantom js
+    // http://stackoverflow.com/questions/2494213/changing-window-location-without-triggering-refresh
+    window.history.replaceState( {}, '', 'http://localhost:8080/context.html?type=Water');
+
     $fj = $('[data-filterable]').filterjitsu();
+  });
+
+  it('should filter down to 3 items', function () {
     expect($('[data-count]').text()).toBe('3 items');
+  });
+
+  it('should show water items', function () {
+    expect($('[data-filterable][data-type="Water"]')).toBeVisible();
+  });
+
+  it('should hide land items', function () {
+    expect($('[data-filterable][data-type!="Water"]')).toBeHidden();
   });
 });
